1242caa21c68a774e8f2b45d114bc2a8a07b5726
onli <onli@paskuda.biz>
2025-01-22 00:44:21 +0100

    Proxy and cache thumbnail image locally, for better privacy

--- serendipity_event_lazyoutube.php
+++ serendipity_event_lazyoutube.php
@@ -9,6 +9,8 @@ if (IN_serendipity !== true) {
 
 class serendipity_event_lazyoutube extends serendipity_event {
 
+    var $markup_elements = [];
+
     function introspect(&$propbag) {
         global $serendipity;
 
@@ -22,7 +24,9 @@ class serendipity_event_lazyoutube extends serendipity_event {
         ));
         $propbag->add('cachable_events', array('frontend_display' => true));
         $propbag->add('event_hooks',   array('frontend_display' => true,
-                                            'frontend_comment' => true));
+                                            'frontend_comment' => true,
+                                            'external_plugin' => true,
+                                            ));
         $propbag->add('groups', array('MARKUP'));
 
         $this->markup_elements = array(
@@ -88,9 +92,41 @@ class serendipity_event_lazyoutube extends serendipity_event {
                             !($eventData['properties']['ep_disable_markup_' . $this->instance] ?? false) &&
                             !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = $temp['element'];
-                            $eventData[$element] = $this->_apply_markup($eventData[$element]);
+                            $eventData[$element] = $this->apply_markup($eventData[$element]);
                         }
                     }
+                    return true;
+                    break;
+                case 'external_plugin':
+                    $parts      = explode('_', $eventData);
+                    if (count($parts) < 2) {
+                        return false;
+                    }
+                    if ($parts[0] == 'lazyoutubefetch') {
+                        $videoid = $parts[1];
+                        $cache_path = $this->get_cache_path($videoid);
+                        # Check if the thumbnail is already cached.
+                        if (! file_exists($cache_path)) {
+                            # If not we have to fetch and store it
+                            # TODO: Allow this only for whitelisted video ids
+                            $thumbnail_url = 'https://img.youtube.com/vi/' . $videoid .'/hqdefault.jpg';
+                            $image_data = serendipity_request_url($thumbnail_url);
+                            if (! file_exists(dirname($cache_path))) {
+                                mkdir(dirname($cache_path));
+                            }
+                            $fp = fopen($cache_path, 'wb');
+                            fwrite($fp, $image_data);
+                            fclose($fp);
+                        }
+                        # Now it exists, we can just redirect to it. The webserver should deliver it properly
+                        $size = @getimagesize($cache_path);
+                        $mime_type = $size['mime'];
+                        header("Content-type: $mime_type");
+                        header("Content-Length: ". filesize($cache_path));
+                        echo file_get_contents($cache_path);
+                        exit();
+                    }
+                    
                     return true;
                     break;
 
@@ -102,8 +138,32 @@ class serendipity_event_lazyoutube extends serendipity_event {
         }
     }
 
+    function get_cache_path($videoid) {
+        global $serendipity;
+        return $serendipity['serendipityPath'] . PATH_SMARTY_COMPILE . '/serendipity_event_lazytube/' . $videoid .'.jpg';
+    }
     
-    function _apply_markup($text) {
+    function get_cache_url($videoid) {
+        global $serendipity;
+        return $serendipity['baseURL'] . PATH_SMARTY_COMPILE . '/serendipity_event_lazytube/' . $videoid .'.jpg';
+    }
+
+    function getPermaPluginPath() {
+        global $serendipity;
+
+        // Get configured plugin path:
+        $pluginPath = 'plugin';
+        if (isset($serendipity['permalinkPluginPath'])){
+            $pluginPath = $serendipity['permalinkPluginPath'];
+        }
+
+        return $pluginPath;
+    }
+
+
+    # Replaces the regular YouTube iframe with a simple thumbnail
+    function apply_markup($text) {
+        global $serendipity;
         //enable \ as escape-character:
         $text = str_replace('\[', chr(2), $text);
         $search = array(
@@ -112,18 +172,20 @@ class serendipity_event_lazyoutube extends serendipity_event {
                        );   
         $search_elements = count($search);
         for($i = 0; $i < $search_elements; $i++) {
-            $text = preg_replace_callback($search[$i], function ($matches) {
+            $text = preg_replace_callback($search[$i], function ($matches) use ($serendipity) {
                             $videoid = $matches[1];
                             if (isset($matches[2]) && ! empty($matches[2])) {
                                 $params = $matches[2] . '&autoplay=1';
                             } else {
                                 $params = '?autoplay=1';
                             }
+                            # TODO: Whitelist this videoid
+                            $proxy_url = $serendipity['baseURL'] . $serendipity['indexFile'] . '?/' . $this->getPermaPluginPath() . '/lazyoutubefetch_' . $videoid;
                             return '<iframe
                                 width="560"
                                 height="315"
                                 src="https://www.youtube-nocookie.com/embed/' . $videoid .'"
-                                srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/' . $videoid . $params . '><img width=560 height=420 loading=lazy src=https://img.youtube.com/vi/' . $videoid .'/hqdefault.jpg><span>▶</span></a>"
+                                srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/' . $videoid . $params . '><img width=560 height=420 loading=lazy src=' . $proxy_url .'><span>▶</span></a>"
                                 frameborder="0"
                                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen
